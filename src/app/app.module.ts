e2dbd2641b2915041275603d7cc4861e792300e0
Renato Rodrigues <rrodriguez@outlook.com.br>
2018-07-01 20:26:13 -0300

    Aula 26 ngif e ngfor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StudentComponent } from './student/student.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
