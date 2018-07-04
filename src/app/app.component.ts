a4f1b209dc0f76b4c892d8050e9680b934e67122
Renato Rodrigues <rrodriguez@outlook.com.br>
2018-07-03 21:10:24 -0300

    Aula 26 utilizando ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'jad-root',
@@ -7,9 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-    luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'};
-    leia = {name: 'Leia', isJedi: false};
-    han = {name: 'Han Solo', isJedi: false};
-    renato = {name: 'renato', isJedi: true, temple: 'Coruscant'};
+    students: Student[] = [
+        { name: 'Luke', isJedi: true, temple: 'Coruscant' },
+        { name: 'Leia', isJedi: false },
+        { name: 'Han Solo', isJedi: false },
+        { name: 'renato', isJedi: true, temple: 'Coruscant' }
+    ]
 
 }
